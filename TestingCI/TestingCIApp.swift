af8de834990b5071f7e664713a3abc756484bd5c
Mykhaylo Levchuk <mykhaylo.levchuk@fieldcode.com>
2022-01-07 23:35:39 +0100

    initial commit

---
+++ TestingCI/TestingCIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingCIApp.swift
+//  TestingCI
+//
+//  Created by Mykhaylo Levchuk on 07/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestingCIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
